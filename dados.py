012641aa17e5842fcb9303325d46ec233f004c49
Caroline Oliveira <caroline@pet-si.ufrrj.br>
2022-05-27 18:39:24 -0300

    UPDATE: tratamento da resposta exibida

--- dados.py
+++ dados.py
@@ -5,9 +5,9 @@ print("Bem-vindo(a) ao lançamento de dados")
 x = int(input("Quantos dados você deseja lançar? "))    #Quantidade de dados lançados de uma vez
 n = int(input("Quantas faces possui o dado que deseja lançar? "))   #Número de faces que o dado terá sendo 6 o número mínimo de faces de um dado
 
-lancamento = 0
+lancamento = 1
 
-while lancamento < x:
+while lancamento <= x:
     resultado = random.randint(1, n)
-    print(f'Número sorteado = {resultado}')
+    print(f'Dado {lancamento}: número sorteado = {resultado}')
     lancamento = lancamento + 1
